6a82cba8214238b4b6b220ba685e6e98f95947c9
isso2500 <isso2500@student.miun.se>
2026-01-20 19:18:11 +0100

    A lot of changes.
    I reduced the size and changed format of jpg photos.
    Added <picture> elements for all the photos.
    
    npm run build wasn't building my other htmls, that
    is now fixed in vite.config.js and it is now moved
    to root folder, as it should be.
    
    Installed vite-image editor, for normal formats
    and avif.

---
+++ vite moment1/vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+export default defineConfig({
+    
+        build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                photos: resolve(__dirname, "photos.html"),
+                about: resolve(__dirname, "about.html")
+            }
+        }
+    },
+    plugins: [
+        ViteImageOptimizer({
+            jpg: {
+                quality: 85
+            },
+            jpeg: {
+                quality: 85
+            },
+
+            webp: {
+                quality: 70
+            },
+            avif: {
+                quality: 60
+            },
+
+        })
+    ]
+});
